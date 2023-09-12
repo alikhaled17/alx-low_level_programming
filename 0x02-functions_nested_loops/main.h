7c39825e4c8449fc3141b0d2e3efb696128a68b8
alikhaled17 <alikaled1x@gmail.com>
2023-09-12 15:54:28 -0700

    create

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,1 @@
-#ifndef main_h
-#define main_h
-
-/**
- * main - Entry point
- *
- * Description: 'the program's description'
- * @parameter: describe the parameter
- *
- * Return: Always 0 (Success)
- */
-
-void _putchar(char);
-#endif
+int _putchar(int c);
