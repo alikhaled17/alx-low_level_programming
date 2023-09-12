c10f3015636dc20c9a4777dc5ece7a0ce62f103d
alikhaled17 <alikaled1x@gmail.com>
2023-09-12 15:48:38 -0700

    create

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,3 +12,4 @@ int main(void)
 	_putchar(10);
 	return (0);
 }
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,14 @@
 #ifndef main_h
 #define main_h
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
 void _putchar(char);
 #endif
